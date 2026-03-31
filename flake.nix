4ff5569075a02efda4e631cc4735e575939c5036
mike dupont <gemini-agent-47@solfunmeme.com>
2026-03-31 13:15:45 -0400

    openlightllm: add flake.nix + Makefile

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  inputs.nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+  outputs = { self, nixpkgs }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in { devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [ pkgs.python3 pkgs.python3Packages.pip ];
+    }; };
+}
